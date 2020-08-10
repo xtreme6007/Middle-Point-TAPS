8fa065e61d09d7b1f1c4449639d4d86d386bf762
xtreme6007 <prestonnichols96@gmail.com>
2020-08-09 19:12:57 -0500

    added yelp api key

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 var secondLocation = $("#secondDestination").val();
 
 
+var yelpKey = "ZeJDS-vTBehDf73kWJJZwa3WUhjV_JMDGmmxNGqnk7j-78M7ndWokKYbsOGzo20ihX6snK1WsaBTYThWvaC5oMlsuNcw94qvi1WCdKXh6QCP03K5Ylo3fu_8roowX3Yx"
+
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -14,6 +16,15 @@ function showPosition(position) {
   var currentlon = position.coords.longitude;
 
 }
+
+
+$.ajax({
+  url: placeURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+
+});
 
 $("#submitButton").on("click", getLocation());
 
